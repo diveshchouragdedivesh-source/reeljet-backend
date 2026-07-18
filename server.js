55413a96c004113e05c064ae8fcdba20e3e94164
diveshchouragdedivesh-source <diveshchouragdedivesh@gmail.com>
2026-07-18 22:53:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ app.get('/api/download', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+// 🔥 PORT = 8081 (taaki 8080 se conflict na ho)
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
